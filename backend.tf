b13bd59f76e37cf52333f743c540f01490cd5715
Biswa Karki <biswakarki@biswas-mbp.lan>
2020-02-29 01:13:00 -0500

    changed backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-bishwa"
+    bucket = "spring-helloapp"
     key    = "terraform/dev/helloapp"
     region = "us-east-1"
     profile = "bishwa"
