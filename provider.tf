692fe6675dc76aa82382d1a5f82a8e1238a01a94
Biswa Karki <biswakarki@biswas-mbp.lan>
2020-02-29 21:20:27 -0500

    bishwa

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "us-east-2"
-  #shared_credentials_file = "/home/intelycorellc/.aws/credentials"
-  access_key = "AKIA5HQYJJUR5E7EZ26D"
-  secret_key = "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"
+  shared_credentials_file = "/home/intelycorellc/.aws/credentials"
+  
   profile = "bishwa"
 }
